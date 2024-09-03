d1f87bde52ca6b6a90e2f933eee494df92a74252
takeo-t <takeo-t@118satellite.com>
2024-09-03 23:32:18 +0900

    最高速度の入力を受け付けする

--- TrainControl/Program.cs
+++ TrainControl/Program.cs
@@ -7,8 +7,17 @@
             Console.WriteLine("Enterキーを押してアプリケーションを起動します。");
             Console.ReadLine();
 
+            Console.Write("最高速度を入力してください（デューティー比 0.0 ～ 1.0）: ");
+            double maxDutyCycle;
+            while(!double.TryParse(Console.ReadLine(), out maxDutyCycle) || maxDutyCycle < 0 || maxDutyCycle > 1)
+            {
+                Console.WriteLine("無効な値です。0.0 ～ 1.0 の間で入力してください。");
+            }
+
             using (MotorController motorController = new MotorController())
             {
+                motorController.MaximumDutyCycle = maxDutyCycle;
+
                 Console.WriteLine("走行開始");
 
                 // モーターを前進
